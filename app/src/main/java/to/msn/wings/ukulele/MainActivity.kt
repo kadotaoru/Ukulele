a2829466c21ad1c566f6c58b5a29698ebbf91b19
kadotaoru <daiyaogata898@gmail.com>
2020-09-21 23:51:00 +0900

    initial commit

--- app/src/main/java/to/msn/wings/ukulele/MainActivity.kt
+++ app/src/main/java/to/msn/wings/ukulele/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
     private var screenHeight: Int = 0
     private var diffY: Int = 0
 
-    // 13音m
+    // 13音mj
     private var c: MediaPlayer = MediaPlayer()
     private var cs: MediaPlayer = MediaPlayer()
     private var d: MediaPlayer = MediaPlayer()
